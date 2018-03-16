9b9a95a49a67c530c7111435c90db66fc1ab9c72
Wynn Blevins <wynnblevins@yahoo.com>
2018-03-16 14:08:34 -0400

    Added script tag for firebase to project. Added firebase config object to project code.  Added click handler for submit button.

---
+++ assets/scripts/script.js
@@ -0,0 +1,13 @@
+var config = {
+    apiKey: "AIzaSyAptp0lxGeq1hLjLq5BuHVPJjH4ps3pqt0",
+    authDomain: "traintime-d1d8c.firebaseapp.com",
+    databaseURL: "https://traintime-d1d8c.firebaseio.com",
+    projectId: "traintime-d1d8c",
+    storageBucket: "",
+    messagingSenderId: "931401348591"
+};
+firebase.initializeApp(config);
+
+$("#add-train-button").click(function () {
+    console.log('Hello, World!');
+});
