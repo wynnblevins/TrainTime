158abbd850190c47e1d126c05b8503320f6e0952
Wynn Blevins <wynnblevins@yahoo.com>
2018-03-16 16:12:21 -0400

    Trains are now being pulled from firebase on load.

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -8,23 +8,23 @@ var config = {
 };
 firebase.initializeApp(config);
 
-var trains = [
-    {name: 'Santa Fe', destination: 'Phoenix Arizona', frequency: 60, next: null, minutesAway: null},
-    {name: 'Benjamin Britten', destination: 'Liverpool', frequency: 80, next: null, minutesAway: null},
-    {name: 'Danube Express', destination: 'Budapest', frequency: 30, next: null, minutesAway: null},
-    {name: 'Allersberg Express', destination: 'Allersburg', frequency: 20, next: null, minutesAway: null},
-    {name: 'Sibirjak', destination: 'Berlin', frequency: 10, next: null, minutesAway: null},
-    {name: 'Polar Express', destination: 'North Pole', frequency: 100, next: null, minutesAway: null},
-    {name: 'Rheingold Express', destination: 'Amsterdam', frequency: 50, next: null, minutesAway: null},
-    {name: 'Vindobona', destination: 'Hamburg', frequency: 15, next: null, minutesAway: null},
-    {name: 'Bernina Express', destination: ' St. Moritz', frequency: 17, next: null, minutesAway: null},
-    {name: 'Gottardo', destination: 'Milan', frequency: 120, next: null, minutesAway: null}
-];
+// var trains = [
+//     {name: 'Santa Fe', destination: 'Phoenix Arizona', frequency: 60, next: null, minutesAway: null},
+//     {name: 'Benjamin Britten', destination: 'Liverpool', frequency: 80, next: null, minutesAway: null},
+//     {name: 'Danube Express', destination: 'Budapest', frequency: 30, next: null, minutesAway: null},
+//     {name: 'Allersberg Express', destination: 'Allersburg', frequency: 20, next: null, minutesAway: null},
+//     {name: 'Sibirjak', destination: 'Berlin', frequency: 10, next: null, minutesAway: null},
+//     {name: 'Polar Express', destination: 'North Pole', frequency: 100, next: null, minutesAway: null},
+//     {name: 'Rheingold Express', destination: 'Amsterdam', frequency: 50, next: null, minutesAway: null},
+//     {name: 'Vindobona', destination: 'Hamburg', frequency: 15, next: null, minutesAway: null},
+//     {name: 'Bernina Express', destination: ' St. Moritz', frequency: 17, next: null, minutesAway: null},
+//     {name: 'Gottardo', destination: 'Milan', frequency: 120, next: null, minutesAway: null}
+// ];
 
 $("#add-train-button").click(function () {
     var trainName = $('#trainName').val();
     var destination = $('#destination').val();
-    var firstTrainTime = $('#firstTrainTime').val();
+    var firstTrainTime = $('#firstTrainTime').val();  // needs to be military time per instructions
     var frequency = $('#frequency').val();
     var dummyVal = '';
     
@@ -32,7 +32,7 @@ $("#add-train-button").click(function () {
         <td>${trainName}</td>
         <td>${destination}</td>
         <td>${frequency}</td>
-        <td>${dummyVal}</td>
+        <td>${firstTrainTime}</td>
         <td>${dummyVal}</td>
     </tr>`;
 
@@ -40,18 +40,23 @@ $("#add-train-button").click(function () {
 });
 
 function init() {
-    
-    var html = '';
-    for(var i = 0; i < trains.length; i++) {
-        html += `<tr>
-            <td>${trains[i].name}</td>
-            <td>${trains[i].destination}</td>
-            <td>${trains[i].frequency}</td>
-            <td>${trains[i].next}</td>
-            <td>${trains[i].minutesAway}</td>
-        </tr>`;
-    }    
-    $('#train-list-table tbody').append(html);
+    // load data from db, put in trains array
+    firebase.database().ref().once('value').then(function (snapshot) {
+        var trainsDb = snapshot.val();
+
+        // build table
+        var html = '';
+        for(var key in trainsDb.trains) {
+            html += `<tr>
+                <td>${trainsDb.trains[key].name}</td>
+                <td>${trainsDb.trains[key].destination}</td>
+                <td>${trainsDb.trains[key].frequency}</td>
+                <td>${trainsDb.trains[key].next}</td>
+                <td>${trainsDb.trains[key].minutesAway}</td>
+            </tr>`;
+        }    
+        $('#train-list-table tbody').append(html);
+    });
 }
 
 init();
